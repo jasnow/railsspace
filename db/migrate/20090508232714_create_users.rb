5c8790130c28f95f5a0c762a1803146a2e6b7d44
U-jasnow-PC\jasnow <jasnow@jasnow-PC.(none)>
2009-05-08 19:34:50 -0400

    end of ch3

---
+++ db/migrate/20090508232714_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.column :screen_name, :string
+      t.column :email, :string
+      t.column :password, :string
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
