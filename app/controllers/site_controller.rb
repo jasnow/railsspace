e5747c381b463a376a4078bec46cff01c4588cbe
U-jasnow-PC\jasnow <jasnow@jasnow-PC.(none)>
2009-05-08 19:09:38 -0400

    initial - end of chapter 2

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,15 @@
+class SiteController < ApplicationController
+
+  def index
+    @title = "RailsSpace"
+  end
+
+  def about
+    @title = "About RailsSpace"
+  end
+
+  def help
+    @title = "RailsSpace Help"
+  end
+
+end
